0168b607a1c42e64a65d2aa44410017527baff6a
odeane_croney <odeane_croney@yahoo.com>
2020-08-05 21:15:12 +0200

    adding header feature test and add json file to hold retrievable data

--- cypress/integration/userCanSeePortfolioStructure.feature.js
+++ cypress/integration/userCanSeePortfolioStructure.feature.js
@@ -1,9 +1,18 @@
 describe('Portfolio interface', () => {
-  it('successfully renders', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+  })
+  
+  it('Page should successfully render a navbar', () => {
     cy.get('.heading').should('contain', 'HOME');
-    cy.get('.heading').should('contain', 'ABOUT'); 
+    cy.get('.heading').should('contain', 'ABOUT');
+    cy.get('.heading').should('contain', 'PROJECTS');
     cy.get('.heading').should('contain', 'RESUME');
     cy.get('.heading').should('contain', 'CONTACT'); 
+  })
+
+  it('Page should successfully render a header', () => {
+    cy.get('h1').should('contain', "Hello, I'm Odeane Croney")
+    cy.get('h3').should('contain', "I'm a full-stack web developer based in Sweden.") 
   })
 })
